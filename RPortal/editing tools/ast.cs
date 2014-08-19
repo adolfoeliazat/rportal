c061750e86c1b5112b303b27675632146a2d76b7
sputterfish <sputterfish@gmail.com>
2014-08-19 14:22:43 -0500

    Updated Applicant Editor to working state.

--- RPortal/editing tools/ast.cs
+++ RPortal/editing tools/ast.cs
@@ -37,82 +37,36 @@ namespace WindowsFormsApplication1
             InitializeComponent();
         }
 
-        private void read_Click(object sender, EventArgs e)// read table by first and last
-        {
-            //adding a reader conn for access to the recruiting db
-            conn.Open();
-
-            cmd.CommandText = "select * from PreHire.dbo.Applicants where fname='" + firstname.Text.Trim() + "' AND lname='" + lastname.Text.Trim() + "'";
-            cmd.Connection = conn;
-            rdr = cmd.ExecuteReader();
-            bool temp = false;
-            while (rdr.Read())
-            {
-                int x;
-                int z;
-                x = rdr.GetInt32(0);
-                z = rdr.GetByte(9);
-                firstname.Text = rdr.GetString(1);
-                lastname.Text = rdr.GetString(2);
-                addy.Text = rdr.GetString(3);
-                city.Text = rdr.GetString(4);
-                state.Text = rdr.GetString(5);
-                zip.Text = rdr.GetString(6);
-                pnumber.Text = rdr.GetString(8);
-                email.Text = rdr.GetString(10);
-                ptft.Text = rdr.GetString(11);
-                shift.Text = rdr.GetString(13);
-               // y = rdr.GetDateTime(3);
-                //rdr.GetDateTime(y);
-                iD.Text = x.ToString();
-                callcenter.Text = z.ToString();
-                //deleted.Text = y.ToString();
-                //iD.Text = rdr.GetString(3);
-                //deleted.Text = rdr.GetString(4);
-                //textBox6.Text = rdr.GetString(5);
-                //textBox7.Text = rdr.GetString(6);
-                //textBox8.Text = rdr.GetString(7);
-                //textBox9.Text = rdr.GetString(8);
-                //textBox10.Text = rdr.GetString(9);
-                temp = true;
-            }
-            if (temp == false)
-                MessageBox.Show("NO ENTRY FOUND", "SQL ERROR");
-            conn.Close();
-            conn.Open();
-            ds = new DataSet();
-            da = new SqlDataAdapter("select * from PreHire.dbo.Applicants", conn);
-            da.Fill(ds, "PreHire.dbo.Applicants");
-            conn.Close();
-        }
-
         private void write_Click(object sender, EventArgs e)// write to table - disabled for now
         {
-            /*
-            //SqlConnection conn = new SqlConnection("server=itdev.corp.telenetwork.com;Database=dttests;User=sa; PWD=gR!FfiN-;");
-            //SqlCommand cmd = new SqlCommand();
+            SqlConnection conn = new SqlConnection("server=sql-prod.corp.telenetwork.com;Database=PreHire;User=sa; PWD=qwerty;");
+            SqlCommand cmd = new SqlCommand();
             SqlDataAdapter dataAdapter = new SqlDataAdapter();
-            //DataSet ds = new DataSet();
-            con.Close();
+            conn.Close();
             try
             {
-                cmd.Connection = con;
-                con.Open();
-                cmd.CommandText = "select fname, lname from dbo.dtable";
+                cmd.Connection = conn;
+                conn.Open();
+                cmd.Parameters.AddWithValue("@id", iD.Text.Trim());
                 cmd.Parameters.AddWithValue("@Fname", firstname.Text);
+                cmd.Parameters.AddWithValue("@Mname", middlename.Text);
                 cmd.Parameters.AddWithValue("@Lname", lastname.Text);
-                cmd.CommandText = "insert into dbo.dtable (fname, lname) values (@Fname, @Lname)";
-                // cmd.Parameters.AddWithValue("@id", id.Text);
+                cmd.Parameters.AddWithValue("@Addy", addy.Text);
+                cmd.Parameters.AddWithValue("@City", city.Text);
+                cmd.Parameters.AddWithValue("@State", state.Text);
+                cmd.Parameters.AddWithValue("@Zip", zip.Text);
+                cmd.Parameters.AddWithValue("@Phone", pnumber.Text);
+                cmd.Parameters.AddWithValue("@SSN", ssn.Text);
+                cmd.Parameters.AddWithValue("@DOB", dob.Text);
+                cmd.CommandText = "UPDATE PreHire.dbo.Background_Check SET fname=@Fname, mname=@Mname, lname=@Lname, addr=@Addy, city=@City, st=@State, zip=@Zip, phone=@Phone, ssn=@SSN, dob=@DOB WHERE appid=@id";
                 cmd.ExecuteNonQuery();
-               // conn.Open();
-                //cmd.ExecuteNonQuery();
-                con.Close();
+                conn.Close();
+                MessageBox.Show("Database Updated.", "Success");
             }
             catch (SqlException)
             {
                 MessageBox.Show("There was a SQL comm error.", "SQL Error");
             }
-             */
         }
 
         private void label6_Click(object sender, EventArgs e)
@@ -132,96 +86,32 @@ namespace WindowsFormsApplication1
 
         private void button1_Click(object sender, EventArgs e)// search by appid
         {
+            conn.Close();
             conn.Open();
-            cmd.CommandText = "select * from PreHire.dbo.Applicants where appid='" + iD.Text.Trim() + "'";
+            cmd.CommandText = "select * from PreHire.dbo.Background_Check where appid='" + iD.Text.Trim() + "'";
             cmd.Connection = conn;
             rdr = cmd.ExecuteReader();
             bool temp = false;
             while (rdr.Read())
             {
                 int x;
-                int z;
-                x = rdr.GetInt32(0);
-                z = rdr.GetByte(9);
+                string dobTime = rdr["dob"].ToString();
+                x = rdr.GetInt32(12);
                 firstname.Text = rdr.GetString(1);
-                lastname.Text = rdr.GetString(2);
-                addy.Text = rdr.GetString(3);
-                city.Text = rdr.GetString(4);
-                state.Text = rdr.GetString(5);
-                zip.Text = rdr.GetString(6);
+                middlename.Text = rdr.GetString(2);
+                lastname.Text = rdr.GetString(3);
+                addy.Text = rdr.GetString(4);
+                city.Text = rdr.GetString(5);
+                state.Text = rdr.GetString(6);
+                zip.Text = rdr.GetString(7);
                 pnumber.Text = rdr.GetString(8);
-                email.Text = rdr.GetString(10);
-                ptft.Text = rdr.GetString(11);
-                shift.Text = rdr.GetString(13);
-                // y = rdr.GetDateTime(3);
-                //rdr.GetDateTime(y);
+                ssn.Text = rdr.GetString(9);
+                dob.Text = dobTime.ToString();
                 iD.Text = x.ToString();
-                callcenter.Text = z.ToString();
-                //deleted.Text = y.ToString();
-                //iD.Text = rdr.GetString(3);
-                //deleted.Text = rdr.GetString(4);
-                //textBox6.Text = rdr.GetString(5);
-                //textBox7.Text = rdr.GetString(6);
-                //textBox8.Text = rdr.GetString(7);
-                //textBox9.Text = rdr.GetString(8);
-                //textBox10.Text = rdr.GetString(9);
                 temp = true;
             }
             if (temp == false)
                 MessageBox.Show("NO ENTRY FOUND", "SQL ERROR");
-            conn.Close();
-            conn.Open();
-            ds = new DataSet();
-            da = new SqlDataAdapter("select * from PreHire.dbo.Applicants", conn);
-            da.Fill(ds, "PreHire.dbo.Applicants");
-            conn.Close();
-        }
-
-        private void button2_Click(object sender, EventArgs e)// search by email
-        {
-            conn.Close();
-            conn.Open();
-            cmd.CommandText = "select * from PreHire.dbo.Applicants where email='" + email.Text.Trim() + "'";
-            cmd.Connection = conn;
-            rdr = cmd.ExecuteReader();
-            bool temp = false;
-            while (rdr.Read())
-            {
-                int x;
-                int z;
-                x = rdr.GetInt32(0);
-                z = rdr.GetByte(9);
-                firstname.Text = rdr.GetString(1);
-                lastname.Text = rdr.GetString(2);
-                addy.Text = rdr.GetString(3);
-                city.Text = rdr.GetString(4);
-                state.Text = rdr.GetString(5);
-                zip.Text = rdr.GetString(6);
-                pnumber.Text = rdr.GetString(8);
-                email.Text = rdr.GetString(10);
-                ptft.Text = rdr.GetString(11);
-                shift.Text = rdr.GetString(13);
-                // y = rdr.GetDateTime(3);
-                //rdr.GetDateTime(y);
-                iD.Text = x.ToString();
-                callcenter.Text = z.ToString();
-                //deleted.Text = y.ToString();
-                //iD.Text = rdr.GetString(3);
-                //deleted.Text = rdr.GetString(4);
-                //textBox6.Text = rdr.GetString(5);
-                //textBox7.Text = rdr.GetString(6);
-                //textBox8.Text = rdr.GetString(7);
-                //textBox9.Text = rdr.GetString(8);
-                //textBox10.Text = rdr.GetString(9);
-                temp = true;
-            }
-            if (temp == false)
-                MessageBox.Show("NO ENTRY FOUND", "SQL ERROR");
-            conn.Close();
-            conn.Open();
-            ds = new DataSet();
-            da = new SqlDataAdapter("select * from PreHire.dbo.Applicants", conn);
-            da.Fill(ds, "PreHire.dbo.Applicants");
             conn.Close();
         }
 
@@ -252,11 +142,13 @@ namespace WindowsFormsApplication1
 
         private void ast_Load(object sender, EventArgs e)
         {
+            /*
             if ((auths.IsAdmin) == (1)) 
             {
                 try { addy.Enabled = true; state.Enabled = true; city.Enabled = true; zip.Enabled = true; pnumber.Enabled = true; write.Enabled = true; ptft.Enabled = true; shift.Enabled = true; callcenter.Enabled = true; }
                 finally { MessageBox.Show("ADMIN POOOOOWER!", "DEBUG INFO"); }
             }
+            */
         }
 
         private void back_Click(object sender, EventArgs e)
